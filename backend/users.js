d7230fd3569382c0c70dbdc0749e48eca034cb46
bradpickering <bradleepickering@gmail.com>
2023-03-25 14:23:14 -0700

    report function finished

--- backend/users.js
+++ backend/users.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const http = require("http");
+const { report } = require("process");
 const server = http.createServer(app);
 
 const usersDb = db.collection("users");
@@ -46,5 +47,23 @@ app.delete("/users/delete_user", async (req, res) => {
     res
       .status(400)
       .json({ error: "User does not exist or password is incorrect" });
+  }
+});
+
+app.get("/users/report/:username", async (req, res) => {
+  const { username } = req.params;
+
+  const user = await usersDb.findOne({ username: username });
+  if (user) {
+    console.log(user);
+    const reportInfo = {
+      "Total Posts": user.totalPosts,
+      "Total Comments": user.totalComments,
+      "Likes Given": user.likesGiven,
+      "Likes Received": user.likesReceived,
+    };
+    res.status(200).json(reportInfo);
+  } else {
+    res.status(400).json({ error: "User not found" });
   }
 });
